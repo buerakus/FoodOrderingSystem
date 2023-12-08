abe6642e7114962c9d20443854fec2a203654d2b
buerakus <112691067+buerakus@users.noreply.github.com>
2023-12-08 16:45:30 +0800

    Update Main.java

--- javaAssignment/src/Main.java
+++ javaAssignment/src/Main.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 
 class Main {
     public static void main(String[] args) {
@@ -12,7 +14,7 @@ class Main {
 
             switch (Sc.nextInt()){
                 case 1:
-                    //login()
+                    login();
                     break;
                 case 2:
                     //register()
@@ -23,6 +25,40 @@ class Main {
             }
         }
     }
+    private static void login() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter your username: ");
+        String username = sc.next();
+        System.out.println("Enter your password: ");
+        String password = sc.next();
+
+        try {
+            Scanner fileScanner = new Scanner(new File("C:\\Users\\lorde\\IdeaProjects\\OODJAssignment\\src\\accounts.txt"));
+            boolean found = false;
+
+            while (fileScanner.hasNextLine()) {
+                String line = fileScanner.nextLine();
+                String[] parts = line.split(",");
+
+                if (parts.length == 2 && parts[0].equals(username) && parts[1].equals(password)) {
+                    System.out.println("Login successful!");
+                    found = true;
+                    break;
+                }
+            }
+
+            if (!found) {
+                System.out.println("Invalid username or password. Please try again.");
+            }
+
+            fileScanner.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Error: accounts.txt not found.");
+        }
+    }
+
 }
+
 //private static void login(){}
 //private static void register(){}
